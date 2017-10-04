ba813e1a1003f737ad53134622140b3a44f6f827
shaark <antoine.2vey@gmail.com>
2017-10-05 00:24:23 +0200

    First release (v0.1)

---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Link = styled.a`
+  color: #1d976c;
+  padding: 8px 16px;
+  font-size: 16px;
+  text-decoration: none;
+  border: 2px solid #fff;
+  border-radius: 25px;
+  background-color: #fff;
+  box-shadow: none;
+  transition: transform 0.2s ease, box-shadow 0.2s ease-in;
+  transform: translateY(0);
+  display: inline-block;
+
+  &:hover {
+    transform: translateY(-3px);
+    box-shadow: 1px 4px 3px #1d976c;
+  }
+`;
+
+const Button = ({ link, children }) => (
+  <div>
+    <Link href={link}>{children}</Link>
+  </div>
+);
+
+export default Button;
