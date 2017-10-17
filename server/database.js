ea80c51e0857d1a12bef2dc5b0d408a77b51e24c
shaark <antoine.2vey@gmail.com>
2017-10-17 12:22:48 +0200

    Merge statistics

--- server/database.js
+++ server/database.js
@@ -7,13 +7,36 @@ const getUsers = async () => {
   const data = await db
     .collection('users')
     .find()
-    .sort({ kebabs: -1 })
     .project({ username: 1, kebabs: 1, firstCount: 1, userId: 1 })
     .toArray();
 
   db.close();
 
-  return data;
+  // temp because it's fucking retarded
+  const properties = ['kebabs', 'firstCount', 'username'];
+  const map = data.reduce((map, e) => {
+    map[e.userId] = properties.map((property, i) => {
+      if (property === 'username') {
+        return e.username;
+      }
+
+      return +e[property] + ((map[e.userId] || [])[i] || 0);
+    });
+
+    return map;
+  }, {});
+
+  const result = Object.keys(map).map(k =>
+    map[k].reduce(
+      (object, e, i) => {
+        object[properties[i]] = e;
+        return object;
+      },
+      { userId: k },
+    ),
+  );
+
+  return result;
 };
 
 module.exports = { getDatabaseInstance, getUsers };
